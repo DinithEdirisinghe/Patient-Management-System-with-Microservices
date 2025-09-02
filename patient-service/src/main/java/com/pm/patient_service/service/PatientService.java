74afb90df9574c8bc21e214838248f40154161b1
Paradoxrc <dinithminindu02@gmail.com>
2025-09-02 13:52:58 +0530

    Implement billing service with gRPC integration, update dependencies, and configure Docker setup

--- patient-service/src/main/java/com/pm/patient_service/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patient_service/service/PatientService.java
@@ -13,15 +13,19 @@ import com.pm.patient_service.exception.PatientNotFoundException;
 import com.pm.patient_service.mapper.PatientMapper;
 import com.pm.patient_service.model.Patient;
 import com.pm.patient_service.repository.PatientRepository;
+import com.pm.patient_service.grpc.BillingServiceGrpcClient;
 
 import jakarta.validation.constraints.Email;
 
 @Service
 public class PatientService {
-    private PatientRepository patientRepository;
+    private final PatientRepository patientRepository;
+    private final BillingServiceGrpcClient billingServiceGrpcClient;
 
-    public PatientService(PatientRepository patientRepository) {
+    public PatientService(PatientRepository patientRepository,
+            BillingServiceGrpcClient billingServiceGrpcClient) {
         this.patientRepository = patientRepository;
+        this.billingServiceGrpcClient = billingServiceGrpcClient;
     }
 
     public List<PatientResponseDTO> getPatients() {
@@ -41,6 +45,10 @@ public class PatientService {
 
         Patient newPatient = patientRepository.save(PatientMapper.toModel(patientRequestDTO));
 
+        billingServiceGrpcClient.createBillingAccount(
+                newPatient.getId().toString(),
+                newPatient.getName(),
+                newPatient.getEmail());
         return PatientMapper.toDTO(newPatient);
     }
 
