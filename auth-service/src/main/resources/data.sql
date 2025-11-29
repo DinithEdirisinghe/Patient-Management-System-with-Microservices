a36490457d85cf2a6b50bfe576590f707a148133
Paradoxrc <dinithminindu02@gmail.com>
2025-11-29 23:18:14 +0530

    47.Add Auth Service with Docker support, database configuration, and user model

---
+++ auth-service/src/main/resources/data.sql
@@ -0,0 +1,20 @@
+-- Ensure the 'users' table exists
+CREATE TABLE IF NOT EXISTS "users" (
+    id UUID PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(50) NOT NULL
+);
+
+-- Insert the user if no existing user with the same id or email exists
+INSERT INTO "users" (id, email, password, role)
+SELECT '55555555-5555-5555-5555-555555555555', 'testuser5@test.com',
+       '$2b$12$7hoRZfJrRKD2nIm2vHLs7OBETy.LWenXXMLKf99W8M4PUwO6KB7fu', 'ADMIN'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM "users"
+    WHERE id = '55555555-5555-5555-5555-555555555555'
+       OR email = 'testuser5@test.com'
+);
+
+
