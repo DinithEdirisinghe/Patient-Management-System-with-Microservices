5dec8f4fd904c8350cc033bb78273ae7f7e92afb
Paradoxrc <dinithminindu02@gmail.com>
2025-12-06 21:54:25 +0530

    Refactor AuthIntegrationTest to improve readability and maintainability

--- integration-tests/src/test/java/AuthIntegrationTest.java
+++ integration-tests/src/test/java/AuthIntegrationTest.java
@@ -6,51 +6,52 @@ import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+//TEST
 public class AuthIntegrationTest {
 
-    @BeforeAll
-    static void setUp() {
-        RestAssured.baseURI = "http://localhost:4004";
-    }
+  @BeforeAll
+  static void setUp() {
+    RestAssured.baseURI = "http://localhost:4004";
+  }
 
-    @Test
-    public void shouldReturnOKWithValidToken() {
-        String loginPayload = """
-                  {
-                    "email": "testuser99@test.com",
-                    "password": "password"
-                  }
-                """;
+  @Test
+  public void shouldReturnOKWithValidToken() {
+    String loginPayload = """
+          {
+            "email": "testuser99@test.com",
+            "password": "password"
+          }
+        """;
 
-        Response response = given()
-                .contentType("application/json")
-                .body(loginPayload)
-                .when()
-                .post("/auth/login")
-                .then()
-                .statusCode(200)
-                .body("token", notNullValue())
-                .extract()
-                .response();
+    Response response = given()
+        .contentType("application/json")
+        .body(loginPayload)
+        .when()
+        .post("/auth/login")
+        .then()
+        .statusCode(200)
+        .body("token", notNullValue())
+        .extract()
+        .response();
 
-        System.out.println("Generated Token: " + response.jsonPath().getString("token"));
-    }
+    System.out.println("Generated Token: " + response.jsonPath().getString("token"));
+  }
 
-    @Test
-    public void shouldReturnUnauthorizedOnInvalidLogin() {
-        String loginPayload = """
-                  {
-                    "email": "invalid_user@test.com",
-                    "password": "wrongpassword"
-                  }
-                """;
+  @Test
+  public void shouldReturnUnauthorizedOnInvalidLogin() {
+    String loginPayload = """
+          {
+            "email": "invalid_user@test.com",
+            "password": "wrongpassword"
+          }
+        """;
 
-        given()
-                .contentType("application/json")
-                .body(loginPayload)
-                .when()
-                .post("/auth/login")
-                .then()
-                .statusCode(401);
-    }
+    given()
+        .contentType("application/json")
+        .body(loginPayload)
+        .when()
+        .post("/auth/login")
+        .then()
+        .statusCode(401);
+  }
 }
